3ea98e46a4da5f67f8afe84dd830932ab71910c7
hostejs <ahostetler@gmail.com>
2024-06-18 18:58:13 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ mkfs.fat -F 32 /dev/sda1;
 mkfs.ext4 -f /dev/sda2
 mount /dev/sda2 /mnt
 mount /dev/sda1 /mnt/boot
-pacstrap -K /mnt base linux linux-firmware nano networkmanager dialog wpa_supplicant xorg-server qemu-desktop
+pacstrap -K /mnt base linux linux-firmware nano networkmanager dialog wpa_supplicant xorg-server qemu-desktop ovmf
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
@@ -18,7 +18,8 @@ hwclock --systohc
 pacman -Syy
 
 # qemu-system-x86_64 -display :0
-pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
+# pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
+# https://ratfactor.com/dwm
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
@@ -43,3 +44,9 @@ usermod -aG wheel hoste
 
 # things to check
 # windowing systems, zram setup, distrobox and qemu/virt-manager setup, acpi and battery status stuff, networking setup, status of mem use, disk space
+
+while xsetroot -name "$(acpi -b | awk 'NR != 2 {print $4}') | $(free -h | awk '/^Mem/ {print $3}') | $(df -h / | awk '/^\/dev/ {print $3}') | $(uptime | sed 's/.*,//') | $(date '+%m/%d/%Y %H:%M')"
+do
+  	sleep 1
+done &
+exec dwm
